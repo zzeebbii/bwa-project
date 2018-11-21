e3378a9f97ba366f1ffbcefc3ba8ab61da603064
Usama Rafique <usamaraf@gmail.com>
2018-11-21 23:41:25 +0200

    All Models have been created

--- user/models.py
+++ user/models.py
@@ -8,3 +8,23 @@ class Profile(models.Model):
     country=models.TextField(max_length=100, blank=True)
     city=models.TextField(max_length=100, blank=True)
     phone=models.TextField(max_length=20, blank=True)
+
+class Friendship(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    req_from=models.IntegerField()
+    req_to=models.IntegerField()
+    is_accepted=models.SmallIntegerField()
+    datetime=models.DateTimeField()
+
+class Status(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    status_text=models.TextField()
+    status_time=models.DateTimeField()
+    is_public=models.BooleanField()
+    likes=models.TextField()
+
+class Comments(models.Model):
+    status=models.ForeignKey(Status, on_delete=models.CASCADE)
+    comment_text=models.TextField()
+    comment_by=models.IntegerField()
+    datetime=models.DateTimeField()
--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path
 from .views import profile
 urlpatterns = [
-    path(r'/profile', profile, name='profile')
+    path(r'profile', profile, name='profile')
 ]
