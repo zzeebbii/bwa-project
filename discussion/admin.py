c4a71d8e9a28eb54314cc8b7bf35cc28fc2285a6
Zohaib Aslam <zohaibse011@gmail.com>
2018-12-08 23:40:28 +0200

    Models are added on admin side CRUD

--- discussion/admin.py
+++ discussion/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from discussion.models import Discussions, DiscussionComments
+
+
 # Register your models here.
 
 
-admin.site.register(Discussions)
-admin.site.register(DiscussionComments)
+# Register your models here.
+class DiscussionAdmin(admin.ModelAdmin):
+    list_display = ['topic', 'text', 'created_by', 'created_at', 'updated_at']
+
+
+class DiscussionCommentAdmin(admin.ModelAdmin):
+    list_display = ['discussion_id', 'text', 'created_at']
+
+
+admin.site.register(Discussions, DiscussionAdmin)
+admin.site.register(DiscussionComments, DiscussionCommentAdmin)
--- friendship/admin.py
+++ friendship/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from friendship.models import Friendship
+
 
 # Register your models here.
+
+
+# Register your models here.
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ['req_from', 'req_to', 'is_accepted', 'created_at', 'updated_at']
+
+
+admin.site.register(Friendship, FriendshipAdmin)
